18c5a972b515f90380c381a9f6e4bfe10652c858
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2022-09-27 20:59:49 +0600

    rest countries

--- countries.js
+++ countries.js
@@ -1,1 +1,27 @@
 console.log("Its working babe");
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+const displayCountries = countries => {
+    console.log(countries);
+    const allCountries = countries.map(country => getCountryHtml(country));
+    // console.log(allCountries[0]);
+    const getCountry = document.getElementById('countries');
+
+    getCountry.innerHTML = allCountries.join(' ');
+
+}
+
+const getCountryHtml = country => {
+    return `
+    <div class = "country">
+        <h2>${country.name.common}</h2>
+        <img src="${country.flags.png}"> 
+        
+
+    </div>
+    `
+}
+loadCountries();
